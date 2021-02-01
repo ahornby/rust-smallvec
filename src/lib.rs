708e6f09ced4ed211a7c63f53bce7dfe2bbda478
Alex Hornby <ahornby@fb.com>
2021-02-01 00:55:35 -0800

    const_generics needs min_const_generics to compile

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,7 @@
 #![cfg_attr(feature = "specialization", allow(incomplete_features))]
 #![cfg_attr(feature = "specialization", feature(specialization))]
 #![cfg_attr(feature = "may_dangle", feature(dropck_eyepatch))]
+#![cfg_attr(feature = "const_generics", feature(min_const_generics))]
 #![deny(missing_docs)]
 
 #[doc(hidden)]
